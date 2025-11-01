d12c24226e25cce5dde4662b478bf8421294a30d
ToxicOtter <luan.lima.lopes16@gmail.com>
2025-11-01 16:06:28 -0300

    Remove chaves do código principal

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart' show kIsWeb, defaultTargetPlatform, TargetPlatform;
+import 'secrets.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
@@ -18,22 +19,22 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyDTiIEcfAowhotI6eoYfwhpaJg_DQGgrj4',
-    appId: '1:1078474745423:web:745745dd5756b9b3546c3a',
-    messagingSenderId: '1078474745423',
-    projectId: 'tccc-3e7d6',
-    authDomain: 'tccc-3e7d6.firebaseapp.com',
-    storageBucket: 'tccc-3e7d6.firebasestorage.app',
-    measurementId: 'G-0TZQCEZYM4',
+    apiKey: Secrets.apiKey,
+    authDomain: Secrets.authDomain,
+    projectId: Secrets.projectId,
+    storageBucket: Secrets.storageBucket,
+    messagingSenderId: Secrets.messagingSenderId,
+    appId: Secrets.appId,
+    measurementId: Secrets.measurementId,
   );
 
    static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDTiIEcfAowhotI6eoYfwhpaJg_DQGgrj4',
-    appId: '1:1078474745423:web:745745dd5756b9b3546c3a',
-    messagingSenderId: '1078474745423',
-    projectId: 'tccc-3e7d6',
-    authDomain: 'tccc-3e7d6.firebaseapp.com',
-    storageBucket: 'tccc-3e7d6.firebasestorage.app',
-    measurementId: 'G-0TZQCEZYM4',
+    apiKey: Secrets.apiKey,
+    authDomain: Secrets.authDomain,
+    projectId: Secrets.projectId,
+    storageBucket: Secrets.storageBucket,
+    messagingSenderId: Secrets.messagingSenderId,
+    appId: Secrets.appId,
+    measurementId: Secrets.measurementId,
   );
 }
